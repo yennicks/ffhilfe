c64612930cb44d2067d09b7d3d4bad770c9b3b18
Yennick Schepers <yennicks@users.noreply.github.com>
2019-09-25 22:06:51 +0200

    Change the output name to a generator so multiple names can be generated to prevent duplicated names.

--- src/ffhilfe/transcode/helpers.py
+++ src/ffhilfe/transcode/helpers.py
@@ -3,15 +3,23 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from pathlib import Path
+from ffhilfe.core.exception import FileExists
 from .settings import get_settings
-
 
 def output_name(file: Path) -> str:
     stem = file.stem
     output_format = get_settings(file).get('output_format')
-    return f'{stem}.{output_format}'
+    yield f'{stem}.{output_format}'
+    for i in range(999):
+        i = str(i).zfill(3)
+        yield f'{stem}_{i}.{output_format}'
 
 
 def output_file(file: Path) -> Path:
     directory = file.parent
-    return directory.joinpath(output_name(file))
+    for o in output_name(file):
+        target = directory.joinpath(o)
+        if not target.exists():
+            return target
+
+    raise FileExists('Could not create a valid output file because the filename is not unique.')
